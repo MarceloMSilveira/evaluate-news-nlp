5fa63db48749f7f1527b148b3157f7ed09d2f055
MarceloMSilveira <marcelomendes.silveira@gmail.com>
2022-11-07 13:24:05 -0300

    initial

---
+++ src/client/js/formHandler.js
@@ -0,0 +1,55 @@
+function handleSubmit(event) {
+    event.preventDefault()
+
+    // check what text was put into the form field
+    let formText = document.getElementById('name').value
+    Client.checkForName(formText)
+
+    console.log("::: Form Submitted! :::")
+    fetch('http://localhost:8081/test')
+    .then(res => res.json())
+    .then(function(res) {
+        document.getElementById('results').innerHTML = res.message
+    })
+
+    //New code:
+    const baseURL = "https://api.meaningcloud.com/sentiment-2.1";
+    const apiKey = '20489377c5952a89f770375f1b73862b';
+    const returnedData = {content:''};
+
+    const formdata = new FormData();
+    formdata.append("key", apiKey);
+    formdata.append("txt", "I am not sure if i need your money, what do you think?");
+    formdata.append("lang", "en");  // 2-letter code, like en es fr ...
+  
+    const requestOptions = {
+      method: 'POST',
+      body: formdata,
+      redirect: 'follow'
+    };
+  
+    retrieveData(baseURL,requestOptions)
+    .then(function(data){
+      console.log(data);
+      returnedData.content = data.subjectivity;
+      console.log(returnedData.content);
+      //postUpdatUI();
+    })
+
+    // Async GET
+    const retrieveData = async (url='', reqOptions) => { 
+        const request = await fetch(url, reqOptions);
+        try {
+            // Transform into JSON
+            const allData = await request.json();
+            return allData;
+        }
+        catch(error) {
+            console.log("error", error);
+            // appropriately handle the error
+        }
+};
+
+}
+
+export { handleSubmit }
---
+++ src/server/index.js
@@ -0,0 +1,34 @@
+var path = require('path')
+const express = require('express')
+const mockAPIResponse = require('./mockAPI.js')
+const MeaningCloud = require('meaning-cloud')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const app = express()
+
+// MeaningCloudApi
+/*const textapi = MeaningCloud({
+   key: process.env.API_KEY
+})*/
+
+console.log(`Your API key is ${process.env.API_KEY}`)
+
+app.use(express.static('dist'))
+
+console.log(__dirname)
+
+app.get('/', function (req, res) {
+    // res.sendFile('dist/index.html')
+    res.sendFile('dist/index.html')
+})
+
+// designates what port the app will listen to for incoming requests
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
+})
+
+app.get('/test', function (req, res) {
+    res.send(mockAPIResponse)
+})
