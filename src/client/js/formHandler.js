54ca69b88d3b95ea81bfd79248e252f6ef1f740b
MarceloMSilveira <marcelomendes.silveira@gmail.com>
2022-11-07 14:27:13 -0300

    after try call API inside server folder

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -9,48 +9,9 @@ function handleSubmit(event) {
     fetch('http://localhost:8081/test')
     .then(res => res.json())
     .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+        document.getElementById('results').innerHTML = res.time;
     })
-
-    //New code:
-    /*
-    const baseURL = "https://api.meaningcloud.com/sentiment-2.1";
-    const apiKey = '20489377c5952a89f770375f1b73862b';
-    const returnedData = {content:''};
-
-    const formdata = new FormData();
-    formdata.append("key", apiKey);
-    formdata.append("txt", "I am not sure if i need your money, what do you think?");
-    formdata.append("lang", "en");  // 2-letter code, like en es fr ...
   
-    const requestOptions = {
-      method: 'POST',
-      body: formdata,
-      redirect: 'follow'
-    };
-  
-    retrieveData(baseURL,requestOptions)
-    .then(function(data){
-      console.log(data);
-      returnedData.content = data.subjectivity;
-      console.log(returnedData.content);
-      //postUpdatUI();
-    })
-
-    // Async GET
-    const retrieveData = async (url='', reqOptions) => { 
-        const request = await fetch(url, reqOptions);
-        try {
-            // Transform into JSON
-            const allData = await request.json();
-            return allData;
-        }
-        catch(error) {
-            console.log("error", error);
-            // appropriately handle the error
-        }
-};
-*/
 }
 
 export { handleSubmit }
--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,38 @@ const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 const MeaningCloud = require('meaning-cloud')
 const dotenv = require('dotenv')
+const apiKey = process.env.API_KEY
+
+const baseURL = "https://api.meaningcloud.com/sentiment-2.1";
+const returnedData = {content:''};
+
+const formdata = new FormData();
+formdata.append("key", apiKey);
+formdata.append("txt", "I am not sure if i need your money, what do you think?");
+formdata.append("lang", "en");  // 2-letter code, like en es fr ...
+  
+const requestOptions = {
+  method: 'POST',
+  body: formdata,
+  redirect: 'follow'
+};
+
+// Async GET to external API (meaningCloud)
+const retrieveData = async (url='', reqOptions) => { 
+    const request = await fetch(url, reqOptions);
+    console.log('test1');
+    try {
+        // Transform into JSON
+        const allData = await request.json();
+        console.log(allData);
+        return allData;
+    }
+    catch(error) {
+        console.log("error", error);
+        // appropriately handle the error
+    }    
+};
+
 
 dotenv.config()
 
@@ -13,11 +45,11 @@ const app = express()
    key: process.env.API_KEY
 })*/
 
-console.log(`Your API key is ${process.env.API_KEY}`)
+//console.log(`Your API key is ${myKey}`)
 
 app.use(express.static('dist'))
 
-console.log(__dirname)
+//console.log(__dirname)
 
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
@@ -30,5 +62,13 @@ app.listen(8081, function () {
 })
 
 app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
+   retrieveData(baseURL,requestOptions)
+    .then(function(data){
+      console.log(data);
+      returnedData.content = data.subjectivity;
+      console.log(returnedData.content);
+      //postUpdatUI();
+    })
+   
+   res.send(mockAPIResponse)
 })
