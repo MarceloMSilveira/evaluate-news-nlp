dffa5ad25589a23a5aee27294a6f296ee8a3d2b0
MarceloMSilveira <marcelomendes.silveira@gmail.com>
2022-11-08 17:26:09 -0300

    funcionou retrieved data

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,15 +1,30 @@
 function handleSubmit(event) {
-    event.preventDefault()
+  event.preventDefault()
+
+  const baseURL = "https://api.meaningcloud.com/sentiment-2.1";
+  const apiKey = '20489377c5952a89f770375f1b73862b';
+  const returnedData = {content:''};
+
+  const formdata = new FormData();
+  formdata.append("key", apiKey);
+  formdata.append("txt", "I am not sure if i need your money, what do you think?");
+  formdata.append("lang", "en");  // 2-letter code, like en es fr ...
+
+  const requestOptions = {
+    method: 'POST',
+    body: formdata,
+    redirect: 'follow'
+  };
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
     Client.checkForName(formText)
 
-    console.log("::: Form Submitted! :::")
-    fetch('http://localhost:8081/test')
+    console.log("::: Form Submitted :::")
+    fetch(baseURL,requestOptions)
     .then(res => res.json())
     .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+        document.getElementById('results').innerHTML = res.subjectivity;
     })
 
     //New code:
